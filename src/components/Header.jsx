5ff204fdf3ad48374fd34e017c5ccdc44d34f127
Your Name <you@example.com>
2024-07-15 22:16:28 +0545

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Portfolio</h1>
+            <h5>Welcome to my Portfolio</h5>
+        </header>
+    );
+};
+
+export default Header;
