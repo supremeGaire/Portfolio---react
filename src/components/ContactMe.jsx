19c7c72ac382c5089c0f4abd7671eb0195d6723e
Your Name <you@example.com>
2024-07-26 22:21:38 +0545

    mail send through formspree

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -10,10 +10,10 @@ const ContactMe = () => {
             </h2>
 
             <div className="flex justify-between">
-                <form className="contact-form space-y-6 w-3/5 text-lg">
-                    <input type="text" placeholder="Your Name" className="w-full p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500" />
-                    <input type="email" placeholder="Your Email" className="w-full p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500" />
-                    <textarea placeholder="Message" className="w-full h-40 p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500 resize-none"></textarea>
+                <form action="https://formspree.io/f/xqazkebn" method="post" className="contact-form space-y-6 w-3/5 text-lg">
+                    <input type="text" name="name" id="name" placeholder="Your Name" maxLength={50} className="w-full p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500" required />
+                    <input type="email" name="email" id="email" placeholder="Your Email" maxLength={50} className="w-full p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500" required />
+                    <textarea name="message" id="message" placeholder="Message" rows="5" className="w-full p-5 border bg-gray-200 placeholder-gray-400 text-gray-600 focus:border-sky-500 focus:bg-white focus:outline-none focus:ring-2 focus:ring-sky-500 resize-none" required></textarea>
                     <button type="submit" className="send-btn text-white py-3 px-12 rounded-full">Send</button>
                 </form>
 
