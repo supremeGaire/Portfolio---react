fb242ec39c53a3acd275241d00be61e6cb2b5360
Your Name <you@example.com>
2024-07-31 14:19:20 +0545

    latest update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components', // Example alias
+      '@components': '/src/components',
     },
   },
 })
